c12f4f00c93290f15a025289aa6dbdac9a0e25ad
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-11-22 17:35:10 +0300

    fix: Exploring destinations components - content

--- src/data/pages/destinations/index.ts
+++ src/data/pages/destinations/index.ts
@@ -3,14 +3,14 @@ import type {  IconFeature } from '/@src/types'
 export const destinationInfo: IconFeature[] = [
   {
     title: 'Academic Environment and Reputation',
-    text: 'Choosing the right educational destination starts with evaluating the academic environment and reputation of the country. .',
+    text: 'Choosing the right educational destination starts with evaluating the academic environment and reputation of the country.',
     color: 'primary',
     icon: 'ion:book-outline',
     link: '/',
   },
   {
-    title: 'Lifestyle and Cultural Fit',
-    text: 'Studying abroad is not just about academics; it\'s also about embracing a new culture and lifestyle. Assess the lifestyle and cultural fit of the country you\'re considering.',
+    title: 'Lifestyle and Budget',
+    text: 'Studying abroad is not just about academics; it\'s also about embracing a new culture and lifestyle.In addition to academic, cultural, and professional factors, it\'s crucial to consider the financial aspects of studying abroad. Evaluate the cost of living, tuition fees, and available financial aid or scholarships in your potential destination. ',
     color: 'success',
     icon: 'ion:home-outline',
     link: '/',
