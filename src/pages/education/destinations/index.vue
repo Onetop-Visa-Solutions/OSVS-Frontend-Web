bd83f5f1da6f2541b90d1bd4119a1dc14d22bcd9
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-11-22 17:29:44 +0300

    feat: Exploring destinations components

--- src/pages/education/destinations/index.vue
+++ src/pages/education/destinations/index.vue
@@ -4,11 +4,26 @@
   </route>
 
   <script setup lang="ts">
-
+import {destinationInfo} from '../../../data/pages/destinations'
   </script>
 
 <template>
   <div>
     <HeroD />
+    <Section>
+      <Container>
+        <SectionTitle
+          title="Exploring Your Ideal Study Destination"
+          subtitle="Unlocking Your Potential"
+        />
+
+        <StackedSection
+          :features="destinationInfo"
+          image="/assets/illustrations/features/ensuring.svg"
+          dark-image="/assets/illustrations/features/ensuring-dark.svg"
+          rounded
+        />
+      </Container>
+    </Section>
   </div>
 </template>
