d8830f1402c0d1de26e8946639f82b35a913e407
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-11-28 14:31:30 +0300

    fix: Destinations detail page advantages - Poland
    - Poland Database attribute order of advantages got fixed

--- src/stores/country/index.ts
+++ src/stores/country/index.ts
@@ -7,7 +7,7 @@ export const useCountryStore = defineStore("country", () => {
   // Fetch Countries Data
   const fetchCountries = async () => {
     const apiResponse = await fetch(
-      'https://run.mocky.io/v3/96f4c11c-7e82-408d-b787-c439b42c7a9e'
+      'https://run.mocky.io/v3/039f73f2-a5e0-45e3-b081-25e30476e53a'
     )
     const countries = await apiResponse.json()
     allCountries.push(...countries)
