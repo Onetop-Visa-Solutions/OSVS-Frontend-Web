2c9e9456e6e189c09f806c09ae7e0606ae14c90f
davidspeedster <53864495+davidspeedster@users.noreply.github.com>
2023-11-30 11:48:53 +0300

    Updates on content
    Updates on testimonials and why choose us.

--- src/data/pages/company/index.ts
+++ src/data/pages/company/index.ts
@@ -2,24 +2,31 @@ import type { IconFeature } from '/@src/types'
 
 export const ourDifference: IconFeature[] = [
   {
-    title: '10+ Years Experience',
-    text: ' Founded in 2012, we have over a decade of experience assisting clients with visas globally. Our experts stay up to date on constantly changing visa regulations and requirements',
+    title: 'Established Reputation',
+    text: ' Founded in 2019, we have over a decade of experience assisting thousands of clients with visas globally. Our experts stay up to date on constantly changing visa regulations and requirements',
     color: 'primary',
     icon: 'ion:trophy-outline',
     link: '/',
   },
   {
-    title: 'Customized Guidance',
+    title: 'Personalized Service',
     text: 'We take time to understand your unique situation and goals. Our specialists then offer tailored advice and visa options to match your needs.',
     color: 'success',
     icon: 'streamline:interface-validation-check-circle-checkmark-addition-circle-success-check-validation-add-form',
     link: '/',
   },
   {
-    title: 'No Advanced Payments',
-    text: 'We do not require any upfront payments for our visa consultation services. Fees are only due once you are satisfied with the results of our guidance',
+    title: 'Wide range of services',
+    text: 'Whether pursuing visas for visits, education, medical care or more, our expert consultants offer personalized guidance every step of the way - skilled at clearing obstacles so you can readily embark upon your journey.',
+    color: 'success',
+    icon: 'material-symbols:settings-account-box-outline-sharp',
+    link: '/',
+  },
+  {
+    title: 'Availability ',
+    text: 'Our dedicated consultants are readily available around the clock to provide meticulous consultation services for customers via phone, email and social media platforms. With our accessible office open 6 days per week, our team remains steadfastly attendant to inquiries surrounding the clock',
     color: 'yellow',
-    icon: 'uil:money-bill-slash',
+    icon: 'material-symbols:event-available-outline-sharp',
     link: '/',
   },
   {
