91ec9b87f94fe6a88f87576f214c04ad155d4dc8
Onestop <78877931+DagMT@users.noreply.github.com>
2023-12-08 12:26:55 +0300

    fix: Icon visibility Issue

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,6 +1,6 @@
 import { createApp } from '/@src/app'
 import initCookiesPlugin from '/@src/utils/cookies'
-
+import '@purge-icons/generated'
 initCookiesPlugin()
 
 createApp().then(async ({ app, router }) => {
